8714080d33267eb3ec02bee39ac3bdc94fbaa8b7
crosstyan <crosstyan@outlook.com>
2023-05-10 20:55:52 +0800

    it's a symbol not section?

--- simpleBleCentral/Source/OSAL_simpleBLECentral.cpp
+++ simpleBleCentral/Source/OSAL_simpleBLECentral.cpp
@@ -25,7 +25,9 @@
 #include "hci_tl.h"
 
 #if defined ( OSAL_CBTIMER_NUM_TASKS )
-    #include "osal_cbTimer.h"
+
+#include "osal_cbTimer.h"
+
 #endif
 
 /* L2CAP */
@@ -60,25 +62,25 @@
 
 // The order in this table must be identical to the task initialization calls below in osalInitTask.
 const pTaskEventHandlerFn tasksArr[] =
-{
-    LL_ProcessEvent,
-    HCI_ProcessEvent,
-    #if defined ( OSAL_CBTIMER_NUM_TASKS )
-    OSAL_CBTIMER_PROCESS_EVENT( osal_CbTimerProcessEvent ),           // task 3
-    #endif
+    {
+        LL_ProcessEvent,
+        HCI_ProcessEvent,
+        #if defined ( OSAL_CBTIMER_NUM_TASKS )
+        OSAL_CBTIMER_PROCESS_EVENT(osal_CbTimerProcessEvent),           // task 3
+        #endif
 
-    L2CAP_ProcessEvent,
-    GAP_ProcessEvent,
-    GATT_ProcessEvent,
-    SM_ProcessEvent,
-    GAPCentralRole_ProcessEvent,
-    GAPBondMgr_ProcessEvent,
-    GATTServApp_ProcessEvent,
-    SimpleBLECentral_ProcessEvent
-};
+        L2CAP_ProcessEvent,
+        GAP_ProcessEvent,
+        GATT_ProcessEvent,
+        SM_ProcessEvent,
+        GAPCentralRole_ProcessEvent,
+        GAPBondMgr_ProcessEvent,
+        GATTServApp_ProcessEvent,
+        SimpleBLECentral_ProcessEvent
+    };
 
-const uint8 tasksCnt = sizeof( tasksArr ) / sizeof( tasksArr[0] );
-uint16* tasksEvents;
+const uint8 tasksCnt = sizeof(tasksArr) / sizeof(tasksArr[0]);
+uint16 *tasksEvents;
 
 /*********************************************************************
     FUNCTIONS
@@ -93,34 +95,33 @@ uint16* tasksEvents;
 
     @return  none
 */
-void osalInitTasks( void )
-{
-    uint8 taskID = 0;
-    tasksEvents = (uint16*)osal_mem_alloc( sizeof( uint16 ) * tasksCnt);
-    osal_memset( tasksEvents, 0, (sizeof( uint16 ) * tasksCnt));
-    /* LL Task */
-    LL_Init( taskID++ );
-    /* HCI Task */
-    HCI_Init( taskID++ );
-    #if defined ( OSAL_CBTIMER_NUM_TASKS )
-    /* Callback Timer Tasks */
-    osal_CbTimerInit( taskID );
-    taskID += OSAL_CBTIMER_NUM_TASKS;
-    #endif
-    /* L2CAP Task */
-    L2CAP_Init( taskID++ );
-    /* GAP Task */
-    GAP_Init( taskID++ );
-    /* GATT Task */
-    GATT_Init( taskID++ );
-    /* SM Task */
-    SM_Init( taskID++ );
-    /* Profiles */
-    GAPCentralRole_Init( taskID++ );
-    GAPBondMgr_Init( taskID++ );
-    GATTServApp_Init( taskID++ );
-    /* Application */
-    SimpleBLECentral_Init( taskID );
+void osalInitTasks(void) {
+  uint8 taskID = 0;
+  tasksEvents = (uint16 *) osal_mem_alloc(sizeof(uint16) * tasksCnt);
+  osal_memset(tasksEvents, 0, (sizeof(uint16) * tasksCnt));
+  /* LL Task */
+  LL_Init(taskID++);
+  /* HCI Task */
+  HCI_Init(taskID++);
+  #if defined ( OSAL_CBTIMER_NUM_TASKS )
+  /* Callback Timer Tasks */
+  osal_CbTimerInit(taskID);
+  taskID += OSAL_CBTIMER_NUM_TASKS;
+  #endif
+  /* L2CAP Task */
+  L2CAP_Init(taskID++);
+  /* GAP Task */
+  GAP_Init(taskID++);
+  /* GATT Task */
+  GATT_Init(taskID++);
+  /* SM Task */
+  SM_Init(taskID++);
+  /* Profiles */
+  GAPCentralRole_Init(taskID++);
+  GAPBondMgr_Init(taskID++);
+  GATTServApp_Init(taskID++);
+  /* Application */
+  SimpleBLECentral_Init(taskID);
 }
 
 
