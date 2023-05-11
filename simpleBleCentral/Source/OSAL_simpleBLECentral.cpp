dfd066ca6832680bf096ffbd28378aa9448179fb
crosstyan <crosstyan@outlook.com>
2023-05-11 18:38:47 +0800

    refactor

--- simpleBleCentral/Source/OSAL_simpleBLECentral.cpp
+++ simpleBleCentral/Source/OSAL_simpleBLECentral.cpp
@@ -7,6 +7,7 @@
 
 **************************************************************************************************/
 
+#include <array>
 #include "OSAL.h"
 #include "OSAL_Tasks.h"
 #include "ll.h"
@@ -20,30 +21,31 @@
 #include "simpleBLECentral.h"
 
 #if defined ( OSAL_CBTIMER_NUM_TASKS )
+
 #include "osal_cbTimer.h"
+
 #endif
 
 // The order in this table must be identical to the task initialization calls below in osalInitTask.
-const pTaskEventHandlerFn tasksArr[] =
-    {
-        LL_ProcessEvent,
-        HCI_ProcessEvent,
-        #if defined ( OSAL_CBTIMER_NUM_TASKS )
-        OSAL_CBTIMER_PROCESS_EVENT(osal_CbTimerProcessEvent),           // task 3
-        #endif
+// tasksArr is global by the way.
+const pTaskEventHandlerFn tasksArr[] = {
+    LL_ProcessEvent,
+    HCI_ProcessEvent,
+    #if defined ( OSAL_CBTIMER_NUM_TASKS )
+    OSAL_CBTIMER_PROCESS_EVENT(osal_CbTimerProcessEvent),
+    #endif
+    L2CAP_ProcessEvent,
+    GAP_ProcessEvent,
+    GATT_ProcessEvent,
+    SM_ProcessEvent,
+    GAPCentralRole_ProcessEvent,
+    GAPBondMgr_ProcessEvent,
+    GATTServApp_ProcessEvent,
+    SimpleBLECentral_ProcessEvent
+};
 
-        L2CAP_ProcessEvent,
-        GAP_ProcessEvent,
-        GATT_ProcessEvent,
-        SM_ProcessEvent,
-        GAPCentralRole_ProcessEvent,
-        GAPBondMgr_ProcessEvent,
-        GATTServApp_ProcessEvent,
-        SimpleBLECentral_ProcessEvent
-    };
-
-const uint8 tasksCnt = sizeof(tasksArr) / sizeof(tasksArr[0]);
-uint16 *tasksEvents;
+const uint8_t tasksCnt = std::size(tasksArr);
+uint16_t *tasksEvents;
 
 /**
     @fn      osalInitTasks
@@ -55,7 +57,7 @@ uint16 *tasksEvents;
     @return  none
 */
 void osalInitTasks(void) {
-  uint8 taskID = 0;
+  uint8_t taskID = 0;
   tasksEvents = (uint16 *) osal_mem_alloc(sizeof(uint16) * tasksCnt);
   osal_memset(tasksEvents, 0, (sizeof(uint16) * tasksCnt));
   /* LL Task */
